26a613eff3e5c6eac7b0cd8c21fa6d0290413353
Shahisha1 <123805474+Shahisha1@users.noreply.github.com>
2026-05-23 21:44:55 +0545

    Added Input validation and error handling
    Added input validation and error handling for user input.

--- comphrensive/Arg.java
+++ comphrensive/Arg.java
@@ -1,13 +1,19 @@
-public class Arg
-{
-    public static void main(String[] args)
-    {
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
+public class Arg {
+    public static void main(String[] args) {
+        if(args.length != 2){
+            System.out.println("Exactly two numbers are required!");
+            return;
+        }
 
-        int sum = a + b;
-        int product = a * b;
-        System.out.println("Sum: "+sum);
-        System.out.println("Product: "+product);
+        try{
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+
+            System.out.println(a + " + " + b + " = "  + (a+b));
+            System.out.println(a + " x " + b + " = "  + (a*b));
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Invalid Input type!");
+        }
     }
-}                   
+}
