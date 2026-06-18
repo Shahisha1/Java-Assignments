aafa924f5e42b1c4de837485d8df249d3475f82b
Shahisha <shahisha123adh@gmail.com>
2026-06-18 07:10:31 +0545

    updated file

--- Assignment 2/comphrensive/Arg.java
+++ Assignment 2/comphrensive/Arg.java
@@ -1,18 +1,18 @@
 public class Arg {
+
     public static void main(String[] args) {
-        if(args.length != 2){
+        if (args.length != 2) {
             System.out.println("Exactly two numbers are required!");
             return;
         }
 
-        try{
-            int a = Integer.parseInt(args[0]);
+        try {
+            int c = Integer.parseInt(args[0]);
             int b = Integer.parseInt(args[1]);
 
-            System.out.println(a + " + " + b + " = "  + (a+b));
-            System.out.println(a + " x " + b + " = "  + (a*b));
-        }
-        catch (IllegalArgumentException e){
+            System.out.println(c + " + " + b + " = " + (c + b));
+            System.out.println(a + " x " + b + " = " + (c * b));
+        } catch (IllegalArgumentException e) {
             System.out.println("Invalid Input type!");
         }
     }
